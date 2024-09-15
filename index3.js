e40bbb6516552e5a6fbe3d48174a1151b4842281
fauziyahsalsabila <fauziyah03@upi.edu>
2024-09-15 23:48:56 +0700

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,16 @@
+var a = 1;
+var b = 2;
+
+if(a+b>4)
+{
+    console.log(a+b);
+}
+else if(a+b<1)
+{
+    console.log(a+b);
+}
+else
+console.log(a+b);
+
+for(var num=0;num<11;num=num+2)
+    console.log(num);
